075041262285e013750112441b8e58a181c47487
Kamizumi <timtsang0513@gmail.com>
2026-05-21 13:11:09 -0700

    Added two pointer solution

--- ArrayManipulation/RotateBox.py
+++ ArrayManipulation/RotateBox.py
@@ -1,11 +1,14 @@
 class Solution:
-    def rotateTheBox(self, boxGrid: list[list[str]]) -> list[list[str]]:
 
+    def BruteForceRotateTheBox(self, boxGrid: list[list[str]]) -> list[list[str]]:
+        '''
+        Function to brute force solve rotating the box
+        '''
         #Gets values for rows and columns
         rows, cols = len(boxGrid), len(boxGrid[0])
 
-        for r in range(rows - 1, -1, -1):
-            for col in range(cols - 1, -1, -1):
+        for r in reversed(range(rows)):
+            for col in reversed(range(cols)):
                 if boxGrid[r][col] == "#":
                     temp_col = col + 1
                     while temp_col < cols and boxGrid[r][temp_col] == '.': #While the temporary column is < real length and the current cell is a .
@@ -18,9 +21,30 @@ class Solution:
         result = []
         for c in range(cols):
             col = []
-            for r in range(rows - 1, -1 ,-1):
+            for r in reversed(range(rows)):
                 col.append(boxGrid[r][c])
             result.append(col)
+        return result
+    #---------------------------------------------------------------------------------------------------
+    def TwoPointerRotateTheBox(self, boxGrid: list[list[str]]) -> list[list[str]]:
+        rows, cols = len(boxGrid), len(boxGrid[0])
+
+        for r in range(rows):
+            i = cols - 1
+            for c in reversed(range(cols)):
+                if boxGrid[r][c] == '#':
+                    boxGrid[r][c], boxGrid[r][i] = boxGrid[r][i], boxGrid[r][c]
+                    i -= 1
+                elif boxGrid[r][c] == '*':
+                    i = c - 1
+        
+        result = []
+        for c in range(cols):
+            col = []
+            for r in reversed(range(rows)):
+                col.append(boxGrid[r][c])
+            result.append(col)
+
         return result
 
 
@@ -30,14 +54,31 @@ def main():
     ["#",".","*","."],
     ["#","#","*","."]]
 
+   
+
+    sol = Solution()
+    result = sol.TwoPointerRotateTheBox(boxGrid = grid)
+
+    for i in range(len(grid)):
+        print(grid[i])
+    print("")
+    for i in range(len(result)):
+        print(f"{result[i]}")
+
+
+
+    #-------------------------------------
+    '''result = sol.BruteForceRotateTheBox(boxGrid = grid)
+
     for i in range(len(grid)):
         print(grid[i])
     print("\n")
 
-    sol = Solution()
-    result = sol.rotateTheBox(boxGrid = grid)
-
     for i in range(len(result)):
         print(f"{result[i]}")
+    '''
+     #-------------------------------------
 
-main()
+
+if __name__ == "__main__":
+    main()
